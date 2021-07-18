bbd8075a5ccd333663140dbe22fb5274c1540500
Sourav Das <iamsouravdaswade420@gmail.com>
2021-07-18 13:53:39 +0530

    initial commit

---
+++ lib/DayFinder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "DayFinder/version"
+
+module DayFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/DayFinder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DayFinder do
+  it "has a version number" do
+    expect(DayFinder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
