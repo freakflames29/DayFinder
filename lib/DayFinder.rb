c38e63e5e17e436b7efd6bdd8ac6e52ff166e1b8
Sourav Das <iamsouravdaswade420@gmail.com>
2021-07-20 19:37:27 +0530

    updated the gem validations

--- lib/DayFinder.rb
+++ lib/DayFinder.rb
@@ -7,10 +7,16 @@ module DayFinder
 
   # Your code goes here...
   def self.find d,m,year
+    month_31 = [1,3,5,7,8,10,12]
+    month_30 = [11,4,6,9]
+    month_28=[2]
     leap = false
     leap = true if ((year % 4).zero? && (year % 100 != 0)) || (year % 400).zero?
     raise "day is invalid" if (m == 2) && (d >= 30)
     raise "month 2 date invalid" if !leap && m == 2 && d >= 29
+    raise "Invalid date" if (month_31.include?(m) && d >= 32 && d != 0) || (month_30.include?(m) && d >= 31 && d != 0)
+    raise "Day is invalid" if d <= 0
+    raise "Month invalid " if !month_31.include?(m) && !month_30.include?(m) && !month_28.include?(m)
 
     if m > 2
       mo = m
